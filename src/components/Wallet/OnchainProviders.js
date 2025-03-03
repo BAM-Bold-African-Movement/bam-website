4bba5cfaceb91dd75dab10195c99cc66756260d0
Emengkeng <blessedsonlove@gmail.com>
2025-03-03 14:02:21 +0100

    feat: all wallet component created

---
+++ src/components/Wallet/OnchainProviders.js
@@ -0,0 +1,30 @@
+import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { base } from 'viem/chains';
+import { WagmiProvider } from 'wagmi';
+import { NEXT_PUBLIC_CDP_API_KEY } from '../config';
+import { useWagmiConfig } from '../../wagmi';
+
+//type Props = { children: ReactNode };
+
+const queryClient = new QueryClient();
+
+function OnchainProviders({ children }) {
+  const wagmiConfig = useWagmiConfig();
+
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
+          <RainbowKitProvider modalSize="compact">
+            {children}
+          </RainbowKitProvider>
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default OnchainProviders;
