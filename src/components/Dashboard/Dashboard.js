ed2da427078fc8d35d7d33795a19e9ef2fcd6e55
Emengkeng <blessedsonlove@gmail.com>
2025-07-04 13:59:05 +0100

    fix: failure to resolve auth context file path

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import BlogPostForm from './BlogPostForm';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../../contexts/AuthContext';
 import BlogService from '../services/BlogService';
 
 const Dashboard = () => {
